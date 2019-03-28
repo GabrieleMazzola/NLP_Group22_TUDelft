6579ab751c622004d36d7afdeb02d11a152e2004
esilezz <matteo.pocchiari@hotmail.it>
2019-03-28 19:36:33 +0100

    similarity and average word length added

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,105 +1,104 @@
-import json_lines
+import string
 import pandas as pd
+import itertools
+from nltk.corpus import stopwords
+from nltk.corpus import wordnet_ic
+from nltk.corpus import wordnet as wn
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.stem import PorterStemmer
 
-truth = pd.read_json("truth.json")
-instances = pd.read_json("instances.json")
 
-instances['class'] = truth['truthClass']
+def stem_string(filtered_sentence):
+    ps = PorterStemmer()
+    return [ps.stem(item) for item in filtered_sentence]
 
-# id postText postTimestamp postMedia targetTitle targetDescription targetKeywords targetParagraphs targetCaptions
 
-features = pd.DataFrame(columns=['numCharPostTitle', 'numCharArticleTitle', 'numCharArticleDescr', 'numCharArticleKeywords',
-                                 'numCharArticleCaption', 'numCharArticleParagraph', 'diffPostTitleArticleTitle',
-                                 'diffPostTitleArticleDesc', 'diffPostTitleArticleKeywords', 'diffPostTitleArticleParagraph',
-                                 'diffPostTitleArticleCaption', 'diffPostTitlePostImage', 'diffArticleTitleArticleDesc',
-                                 'diffArticleTitleArticleKeywords', 'diffArticleTitleArticleParagraph', 'diffArticleTitleArticleCaption',
-                                 'diffArticleTitlePostImage', 'diffArticleDescArticleKeywords', 'diffArticleDescArticleParagraph',
-                                 'diffArticleDescArticleCaption', 'diffArticleDescPostImage', 'diffArticleKeywordsArticleParagraph',
-                                 'diffArticleKeywordsArticleCaption', 'diffArticleKeywordsPostImage', 'diffArticleParagraphArticleCaption',
-                                 'diffArticleParagraphPostImage', 'diffArticleCaptionPostImage', 'ratioPostTitleArticleTitle',
-                                 'ratioPostTitleArticleDesc', 'ratioPostTitleArticleKeywords', 'ratioPostTitleArticleParagraph',
-                                 'ratioPostTitleArticleCaption', 'ratioPostTitlePostImage', 'ratioArticleTitleArticleDesc',
-                                 'ratioArticleTitleArticleKeywords', 'ratioArticleTitleArticleParagraph', 'ratioArticleTitleArticleCaption',
-                                 'ratioArticleTitlePostImage', 'ratioArticleDescArticleKeywords', 'ratioArticleDescArticleParagraph',
-                                 'ratioArticleDescArticleCaption', 'ratioArticleDescPostImage', 'ratioArticleKeywordsArticleParagraph',
-                                 'ratioArticleKeywordsArticleCaption', 'ratioArticleKeywordsPostImage', 'ratioArticleParagraphArticleCaption',
-                                 'ratioArticleParagraphPostImage', 'ratioArticleCaptionPostImage'])
+def clean_string(sentence):
+    stop_words = set(stopwords.words('english'))
 
-for index, row in instances.iterrows():
-    ### NUMBER OF CHARACTERS
+    sentence = sentence.translate(str.maketrans('', '', string.punctuation))
+    word_tokens = word_tokenize(sentence)
+    filtered_sentence = [w for w in word_tokens if not w in stop_words]
+    filtered_sentence = []
+    for w in word_tokens:
+        if w not in stop_words:
+            filtered_sentence.append(w)
+    return filtered_sentence
+
+
+def avg_similarity(sentence):
+    semcor_ic = wordnet_ic.ic('ic-semcor.dat')
+    filtered_sentence = clean_string(sentence)
+    # filtered_sentence = stem_string(filtered_sentence)
+    combi = set(itertools.combinations(filtered_sentence, 2))
+    sum = 0
+    for tup in combi:
+        word1 = wn.synsets(tup[0])
+        if len(word1) > 0:
+            word1 = word1[0]
+        word2 = wn.synsets(tup[1])
+        if len(word2) > 0:
+            word2 = word2[0]
+        if (word1 and word2) and word1._pos == word2._pos and word1._pos != 's' and word2._pos != 's' and \
+                word1._pos != 'a' and word2._pos != 'a' and word1._pos != 'r' and word2._pos != 'r':
+            sum = sum + word1.lin_similarity(word2, semcor_ic)
+    avg = sum / len(combi)
+    return avg
+
+
+def avg_word_length(sentence):
+    filtered_sentence = clean_string(sentence)
+    sum = 0
+    for word in filtered_sentence:
+        sum = sum + len(word)
+    avg = sum / len(filtered_sentence)
+    return avg
+
+
+def char_based_features(row, features_dict):
     lenPostTitle = len(row['postText'][0])
     lenArtTitle = len(row['targetTitle'])
     lenArtDesc = len(row['targetDescription'])
     lenArtKeywords = len(row['targetKeywords'])
-    print(len(row['postText'][0].split()))
-    features_dict = {}
-    features_dict['numCharPostTitle'] = [lenPostTitle]
+    # print(len(row['postText'][0].split()))
+    features_dict['numCharPostTitle'] = lenPostTitle
     # MISSING THE NUMBER OF CHARACTERS FROM POST'S IMAGE
-    features_dict['numCharArticleTitle'] = [lenArtTitle]
-    features_dict['numCharArticleDescr'] = []
-    features_dict['numCharArticleKeywords'] = [lenArtKeywords]
+    features_dict['numCharArticleTitle'] = lenArtTitle
+    features_dict['numCharArticleDescr'] = lenArtDesc
+    features_dict['numCharArticleKeywords'] = lenArtKeywords
 
     lenArtCap = 0
     for caption in row['targetCaptions']:
         lenArtCap = lenArtCap + len(caption)
-    features_dict['numCharArticleCaption'] = [lenArtCap]
+    features_dict['numCharArticleCaption'] = lenArtCap
 
     lenArtPar = 0
     for paragraph in row['targetParagraphs']:
         lenArtPar = lenArtPar + len(paragraph)
-    features_dict['numCharArticleParagraph'] = [lenArtPar]
-
-    ### DIFFERENCE IN CHARACTERS - STILL TO DO THE CHECK OF THE ZERO CHARACTER CASE
-    features_dict['diffPostTitleArticleTitle'] = [abs(lenPostTitle-lenArtTitle)]
-    features_dict['diffPostTitleArticleDesc'] = [abs(lenPostTitle-lenArtDesc)]
-    features_dict['diffPostTitleArticleKeywords'] = [abs(lenPostTitle-lenArtKeywords)]
-    features_dict['diffPostTitleArticleParagraph'] = [abs(lenPostTitle-lenArtPar)]
-    features_dict['diffPostTitleArticleCaption'] = [abs(lenPostTitle-lenArtCap)]
-    # STILL MISSING features_dict['diffPostTitlePostImage'] = [abs(lenPostTitle-lenPostImage)]
-    features_dict['diffArticleTitleArticleDesc'] = [abs(lenArtTitle-lenArtDesc)]
-    features_dict['diffArticleTitleArticleKeywords'] = [abs(lenArtTitle-lenArtKeywords)]
-    features_dict['diffArticleTitleArticleParagraph'] = [abs(lenArtTitle-lenArtPar)]
-    features_dict['diffArticleTitleArticleCaption'] = [abs(lenArtTitle-lenArtCap)]
-    # STILL MISSING features_dict['diffArticleTitlePostImage'] = [abs(lenArtTitle-lenPostImage)]
-    features_dict['diffArticleDescArticleParagraph'] = [abs(lenArtDesc-lenArtPar)]
-    features_dict['diffArticleDescArticleKeywords'] = [abs(lenArtDesc-lenArtKeywords)]
-    features_dict['diffArticleDescArticleCaption'] = [abs(lenArtDesc-lenArtCap)]
-    # STILL MISSING features_dict['diffArticleDescPostImage'] = [abs(lenArtDesc-lenPostImage)]
-    features_dict['diffArticleKeywordsArticleParagraph'] = [abs(lenArtKeywords-lenArtPar)]
-    features_dict['diffArticleKeywordsArticleCaption'] = [abs(lenArtKeywords-lenArtCap)]
-    # STILL MISSING features_dict['diffArticleKeywordsPostImage'] = [abs(lenArtKeywords-lenPostImage)]
-    features_dict['diffArticleParagraphArticleCaption'] = [abs(lenArtPar-lenArtCap)]
-    # STILL MISSING features_dict['diffArticleParagraphPostImage'] = [abs(lenArtPar-lenPostImage)]
-    # STILL MISSING features_dict['diffArticleCaptionPostImage'] = [abs(lenArtCap-lenPostImage)]
+    features_dict['numCharArticleParagraph'] = lenArtPar
 
 
-    ### RATIO IN CHARACTERS - STILL TO DO THE CHECK OF THE ZERO CHARACTER CASE
-    features_dict['ratioPostTitleArticleTitle'] = [1/abs(lenPostTitle/lenArtTitle)]
-    features_dict['ratioPostTitleArticleDesc'] = [1/abs(lenPostTitle/lenArtDesc)]
-    features_dict['ratioPostTitleArticleKeywords'] = [1/abs(lenPostTitle/lenArtKeywords)]
-    features_dict['ratioPostTitleArticleParagraph'] = [1/abs(lenPostTitle/lenArtPar)]
-    features_dict['ratioPostTitleArticleCaption'] = [1/abs(lenPostTitle/lenArtCap)]
-    # STILL MISSING features_dict['ratioPostTitlePostImage'] = [1/abs(lenPostTitle/lenPostImage)]
-    features_dict['ratioArticleTitleArticleDesc'] = [1/abs(lenArtTitle/lenArtDesc)]
-    features_dict['ratioArticleTitleArticleKeywords'] = [1/abs(lenArtTitle/lenArtKeywords)]
-    features_dict['ratioArticleTitleArticleParagraph'] = [1/abs(lenArtTitle/lenArtPar)]
-    features_dict['ratioArticleTitleArticleCaption'] = [1/abs(lenArtTitle/lenArtCap)]
-    # STILL MISSING features_dict['diffArticleTitlePostImage'] = [1/abs(lenArtTitle-lenPostImage)]
-    features_dict['ratioArticleDescArticleParagraph'] = [1/abs(lenArtDesc/lenArtPar)]
-    features_dict['ratioArticleDescArticleKeywords'] = [1/abs(lenArtDesc/lenArtKeywords)]
-    features_dict['ratioArticleDescArticleCaption'] = [1/abs(lenArtDesc/lenArtCap)]
-    # STILL MISSING features_dict['ratioArticleDescPostImage'] = [1/abs(lenArtDesc/lenPostImage)]
-    features_dict['ratioArticleKeywordsArticleParagraph'] = [1/abs(lenArtKeywords/lenArtPar)]
-    features_dict['ratioArticleKeywordsArticleCaption'] = [1/abs(lenArtKeywords/lenArtCap)]
-    # STILL MISSING features_dict['ratioArticleKeywordsPostImage'] = [1/abs(lenArtKeywords/lenPostImage)]
-    features_dict['ratioArticleParagraphArticleCaption'] = [1/abs(lenArtPar/lenArtCap)]
-    # STILL MISSING features_dict['ratioArticleParagraphPostImage'] = [1/abs(lenArtPar/lenPostImage)]
-    # features_dict['ratioArticleCaptionPostImage'] = [1/abs(lenArtCap/lenPostImage)]
+truth = pd.read_json("train_set/truth.json")
+instances = pd.read_json("train_set/instances.json")
 
+instances['class'] = truth['truthClass']
 
-    ### NUMBER OF WORDS
+# id postText postTimestamp postMedia targetTitle targetDescription targetKeywords targetParagraphs targetCaptions
+cols = ['numCharPostTitle', 'numCharArticleTitle', 'numCharArticleDescr', 'numCharArticleKeywords',
+        'numCharArticleCaption', 'numCharArticleParagraph', 'avgSimilarityPostTitle', 'capitalPostTitle',
+        'avgWordLenPostTitle', 'avgWordLenArticleTitle']
 
+features = pd.DataFrame(columns=cols)
 
-    features = features.append(pd.DataFrame.from_dict(features_dict), ignore_index=True)
+for index, row in instances.iterrows():
+    features_dict = {}
+    char_based_features(row, features_dict)
+    features_dict['avgSimilarityPostTitle'] = avg_similarity(row['postText'][0])
+    features_dict['capitalPostTitle'] = sum(1 for c in row['postText'][0] if c.isupper())
+    features_dict['avgWordLenPostTitle'] = avg_word_length(row['postText'][0])
+    features_dict['avgWordLenArticleTitle'] = avg_word_length(row['targetTitle'])
 
+    features = features.append(features_dict, ignore_index=True)
+
+print("mannaccia il cristo appeso")
 print(features)
