20aae11effb99ab416d68a65381a17b9190acddb
Gabriele Mazzola <gabriele.mazzola@techedgegroup.com>
2019-04-05 11:35:34 +0200

    plot conf matrix

--- evaluation/main.py
+++ evaluation/main.py
@@ -1,12 +1,14 @@
 import random
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sn
 from sklearn import preprocessing
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import precision_score, accuracy_score, recall_score, f1_score, mean_squared_error
-from sklearn.model_selection import GridSearchCV, RepeatedStratifiedKFold
+from sklearn.metrics import precision_score, accuracy_score, recall_score, f1_score, mean_squared_error, \
+    confusion_matrix
 from sklearn.model_selection import train_test_split
 
 from gabry_dataset_parser import get_labeled_instances
@@ -17,23 +19,19 @@ PATH_TO_FEATURE_FOLDER = "../features/"
 
 feature_paths = []
 feature_paths.append(('POS postText no-normalized pruned', PATH_TO_FEATURE_FOLDER + "{}/pos_features_{}_postText_no-normalized_infoGain70.0.csv".format(DATASET, DATASET)))
-# alone -> 66.8 % log reg
-feature_paths.append(('POS targetTitle normalized', PATH_TO_FEATURE_FOLDER + "{}/pos_features_{}_targetTitle_normalized.csv".format(DATASET, DATASET)))
-# alone -> 62.7 % log reg
-# POS together -> 67.3%
+
+#feature_paths.append(('POS targetTitle normalized', PATH_TO_FEATURE_FOLDER + "{}/pos_features_{}_targetTitle_normalized.csv".format(DATASET, DATASET)))
+
 
 # feature_paths.append(("Formal postText normalized",  PATH_TO_FEATURE_FOLDER + "{}/formal_informal_features_{}_postText_normalized.csv".format(DATASET, DATASET))) # useless
 # feature_paths.append(("Formal targetTitle normalized",PATH_TO_FEATURE_FOLDER + "{}/formal_informal_features_{}_targetTitle_normalized.csv".format(DATASET, DATASET))) # useless
 
 feature_paths.append(("Matteo features",PATH_TO_FEATURE_FOLDER + "{}/matteo_features_full.csv".format(DATASET)))
-# alone -> 69.4 % (logreg)
 
 
 # feature_paths.append(("Similarity lin", PATH_TO_FEATURE_FOLDER + "{}/matteo_full_similarity.csv".format(DATASET)))
-# alone -> 0 precision
 
 feature_paths.append(("Bianca features", PATH_TO_FEATURE_FOLDER + "{}/bianca_features.csv".format(DATASET)))
-# alone -> 68.7% logreg
 
 feature_paths.append(("Ngrams", "../features/big/ngrams_features_counts_after_infoGain1.0.csv"))
 
@@ -84,9 +82,6 @@ if CHECK_RANDOM:
     random_feat = np.array([random.randint(-1, 1) for _ in range(eval_df.shape[0])])
     eval_df = pd.DataFrame(data=random_feat)
 
-# Split in train (which we use for Grid Search) and test
-X_train, X_test, y_train, y_test = train_test_split(eval_df, label_encoded, test_size=0.2, random_state=42)
-
 param_randForest = {
     'n_estimators': [400],
     'max_depth': [100, 125, 150, 175, 200],
@@ -113,6 +108,9 @@ models.append(("Logistic ", LogisticRegression(solver='liblinear'), param_logist
 eval_metric = 'precision'
 print(f"'{eval_metric}' is being used as evaluation metric")
 
+# Split in train (which we use for Grid Search) and test
+X_train, X_test, y_train, y_test = train_test_split(eval_df, label_encoded, test_size=0.2, random_state=42)
+
 # results = []
 # for model in models:
 #     clf = GridSearchCV(model[1], model[2], cv=5, scoring=eval_metric)
@@ -122,33 +120,75 @@ print(f"'{eval_metric}' is being used as evaluation metric")
 #     print(clf.best_params_, clf.best_score_)
 
 
+# for model_name, model, model_params in models:
+#     print(f"using model {model_name}")
+#     skf = RepeatedStratifiedKFold(n_splits=5, n_repeats=3, random_state=36851234)
+#     precisions = []
+#     accuracies = []
+#     recalls = []
+#     f1s = []
+#     mses = []
+#     for train_index, test_index in skf.split(eval_df, label_encoded):
+#         X_train, X_test = eval_df.loc[train_index], eval_df.loc[test_index]
+#         y_train, y_test = label_encoded.loc[train_index], label_encoded.loc[test_index]
+#         y_medians = medians.loc[test_index]
+#
+#         model.fit(X_train, y_train.values.ravel())
+#         y_pred_hard = model.predict(X_test)
+#         y_pred_soft = model.predict_proba(X_test)[:, 1]
+#         precision = precision_score(y_test, y_pred_hard)
+#         precisions.append(precision)
+#         accuracies.append(accuracy_score(y_test, y_pred_hard))
+#         recalls.append(recall_score(y_test, y_pred_hard))
+#         f1s.append(f1_score(y_test, y_pred_hard))
+#         mses.append(mean_squared_error(y_medians, y_pred_soft))
+#
+#     print(model_name)
+#     print("acc", np.mean(accuracies), np.std(accuracies))
+#     print("prec", np.mean(precisions), np.std(precisions))
+#     print("rec", np.mean(recalls), np.std(recalls))
+#     print("f1", np.mean(f1s), np.std(f1s))
+#     print("mse", np.mean(mses), np.std(mses))
+
+
+print("\n\n----------------- Final test ----------------------")
 for model_name, model, model_params in models:
     print(f"using model {model_name}")
-    skf = RepeatedStratifiedKFold(n_splits=5, n_repeats=3, random_state=36851234)
-    precisions = []
-    accuracies = []
-    recalls = []
-    f1s = []
-    mses = []
-    for train_index, test_index in skf.split(eval_df, label_encoded):
 
-        X_train, X_test = eval_df.loc[train_index], eval_df.loc[test_index]
-        y_train, y_test = label_encoded.loc[train_index], label_encoded.loc[test_index]
-        y_medians = medians.loc[test_index]
+    y_medians = medians.loc[pd.Series(y_test.index)]
 
-        model.fit(X_train, y_train.values.ravel())
-        y_pred_hard = model.predict(X_test)
-        y_pred_soft = model.predict_proba(X_test)[:, 1]
-        precision = precision_score(y_test, y_pred_hard)
-        precisions.append(precision)
-        accuracies.append(accuracy_score(y_test, y_pred_hard))
-        recalls.append(recall_score(y_test, y_pred_hard))
-        f1s.append(f1_score(y_test, y_pred_hard))
-        mses.append(mean_squared_error(y_medians, y_pred_soft))
+    model.fit(X_train, y_train.values.ravel())
+    y_pred_hard = model.predict(X_test)
+    y_pred_soft = model.predict_proba(X_test)[:, 1] # keep only probabilities of being clickbait
 
-    print(model_name)
-    print("acc", np.mean(accuracies), np.std(accuracies))
-    print("prec", np.mean(precisions), np.std(precisions))
-    print("rec", np.mean(recalls), np.std(recalls))
-    print("f1", np.mean(f1s), np.std(f1s))
-    print("mse", np.mean(mses), np.std(mses))
+    precision = precision_score(y_test, y_pred_hard)
+    accuracy = accuracy_score(y_test, y_pred_hard)
+    recall = recall_score(y_test, y_pred_hard)
+    f1 = f1_score(y_test, y_pred_hard)
+    mse = mean_squared_error(y_medians, y_pred_soft)
+
+    print("acc", accuracy)
+    print("prec", precision)
+    print("rec", recall)
+    print("f1", f1)
+    print("mse", mse)
+
+    labels = [0, 1]
+    cm = confusion_matrix(y_test, y_pred_hard, labels)
+    print(cm)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(cm)
+    plt.title('Confusion matrix of the classifier {}'.format(model_name))
+    fig.colorbar(cax)
+    ax.set_xticklabels([''] + labels)
+    ax.set_yticklabels([''] + labels)
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+    plt.show()
+
+    df_cm = pd.DataFrame(cm, index=[i for i in ['true: no-clickbait', 'true: clickbait']],
+                         columns=[i for i in ['pred: no-clickbait', 'pred: clickbait']])
+    plt.figure(figsize=(10, 7))
+    sn.heatmap(df_cm, annot=True, fmt='.1f')
+    plt.show()
